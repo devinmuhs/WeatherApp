73c18e128c7ce46c1120532bb296aca09451a483
Devin Muhs <devinmuhs@gmail.com>
2019-09-12 15:46:10 -0400

    added unit test

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -4,11 +4,33 @@ using Newtonsoft.Json;
 
 namespace WeatherApp
 {
-	class Program
-	{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Welcome To The RDU Weather App!\n\nThis Application uses 30 years worth of data to predict the weather at the Raleigh-Durham International Airport.\n");
+
+                // Request user input for month and assigns the input to a variable.
+                Console.Write("First, please enter the month you would like to check the weather for:\n(Format: January, February, etc.) - (Leave blank for this month): ");
+                string month = Console.ReadLine();
+
+                // Request user input for day and assigns the input to a variable.
+                Console.Write("\nNow, please enter the day of the month you would like to check the weather for:\n(Format: 1, 2, etc.) - (Leave blank for todays' date): ");
+                string day = Console.ReadLine();
+
+                // Calls the WeatherForecast function and assigns it's value to output variable.
+                string output = WeatherForecast.Forecast(month, day);
+
+                // Prints the output to the console.
+                Console.WriteLine(output);
+                Console.WriteLine("Thank you for using the RDU Weather App! Please re-run the application to check another date!");
+            }
+        }
+    }
 		public static class WeatherForecast
 		{
-			// Main forecast function is defined with paramters.
+			// Forecast function is defined with paramters.
 			public static string Forecast(string month, string day)
 			{
 				// Program reads the json file using StreamReader.
@@ -44,27 +66,5 @@ namespace WeatherApp
 				return $"\nThere is no record for {month} {day}. Please ensure your request was formated correctly.\n(Month Format: August, January, etc.) - (Day Format: 4, 30, etc.)\n";
 			}
 		}
-		static void Main(string[] args)
-		{
-			{
-				Console.WriteLine("Welcome To The RDU Weather App!\n\nThis Application uses 30 years worth of data to predict the weather at the Raleigh-Durham International Airport.\n");
-
-				// Request user input for month and assigns the input to a variable.
-				Console.Write("First, please enter the month you would like to check the weather for:\n(Leave blank for this month) - (Format: January, February, etc.): ");
-				string month = Console.ReadLine();
-
-				// Request user input for day and assigns the input to a variable.
-				Console.Write("\nNow, please enter the day of the month you would like to check the weather for:\n(Leave blank for todays' date) - (Format: 1, 2, etc.): ");
-				string day = Console.ReadLine();
-
-				// Calls the WeatherForecast function and assigns it's value to output variable.
-				string output = WeatherForecast.Forecast(month, day);
-
-				// Prints the output to the console.
-				Console.WriteLine(output);
-				Console.WriteLine("Thank you for using the RDU Weather App! Please re-run the application to check another date!");
-			}
-		}
-	}
 }
 
